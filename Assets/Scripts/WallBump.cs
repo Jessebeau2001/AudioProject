eaaaaebb604da53ad991ed9f5c1ce5dcd16fc494
Jessebeau2001 <jessegamen@gmail.com>
2020-11-20 09:38:49 +0100

    Added TestLevel

---
+++ Assets/Scripts/WallBump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBump : MonoBehaviour
+{
+    public AudioSource audioData;
+    public bool audibleBumps = true;
+    void Start()
+    {
+        //audioData = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.collider.tag == "Wall") {
+            audioData.Play(0);
+        }
+    }
+}
+
+// Check for collision with wall
+// Check for collision point
+// Draw vector towards point
+// Nomrlalize it
+// Play directional audio file at end point of vector
