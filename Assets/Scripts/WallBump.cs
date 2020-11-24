5e877efa6c9718abfc43264ffdcc2c9ad13fc43e
Jessebeau2001 <jessegamen@gmail.com>
2020-11-24 14:27:22 +0100

    Wall bumping + Dynamic Audio Prefab

--- Assets/Scripts/WallBump.cs
+++ Assets/Scripts/WallBump.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(AudioSource))]
 public class WallBump : MonoBehaviour
 {
-    public AudioSource audioData;
+    public float distanceMod = 4f;
     public bool audibleBumps = true;
+    public AudioSource src;
     void Start()
     {
         //audioData = GetComponent<AudioSource>();
@@ -13,18 +15,24 @@ public class WallBump : MonoBehaviour
 
     void Update()
     {
-        
-    }
 
-    void OnCollisionEnter(Collision col) {
-        if (col.collider.tag == "Wall") {
-            audioData.Play(0);
-        }
+    }
+        
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "ddddddd")
+            return;
+            
+        ContactPoint contact = collision.contacts[0];
+        Vector3 direction = contact.point - transform.position;
+        direction.y = 0; //Ignore verticality
+        direction.Normalize();
+        Debug.DrawRay(transform.position, direction * distanceMod);
+        AudioSource.PlayClipAtPoint(src.clip, transform.position + (direction * distanceMod));
     }
 }
-
-// Check for collision with wall
-// Check for collision point
-// Draw vector towards point
+// Check for collision with wall    | Check
+// Check for collision points       | Check
+// Draw vector towards point        | Check
 // Nomrlalize it
 // Play directional audio file at end point of vector
