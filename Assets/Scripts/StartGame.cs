836cc98a90a759211ed7e7a3e7e94404888c60d8
Jessebeau2001 <jessegamen@gmail.com>
2020-12-01 16:05:59 +0100

    Added Intro scene
    > Intro scene contains first cutscene
    > Automatically goes to level 1 when cutscene is over

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public AudioClip Audiocutscene1;
+    public float delay = 0;
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(Audiocutscene1, transform.position);
+        StartCoroutine(EnableGame());
+    }
+    
+    IEnumerator EnableGame() {
+        yield return new WaitForSeconds(Audiocutscene1.length + delay);
+        SceneManager.LoadScene(1);
+        Destroy(this);
+    }
+}
